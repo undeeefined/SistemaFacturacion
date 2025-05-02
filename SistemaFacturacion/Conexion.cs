b8fdb296917dc83106ba6d2939237a01afbe5b43
ERVASQUEZ0488 <ric.vasquez011@gmail.com>
2025-05-02 15:09:32 -0600

    Agregué clase conexion de prueba

---
+++ SistemaFacturacion/Conexion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFacturacion
+{
+    internal class Conexion
+    {
+        private readonly string cadena = "Server=Ricardo;Database=FACTURACIONPI;Integrated Security=true;";
+        private SqlConnection conexion;
+
+        public SqlConnection AbrirConexion() {
+            if (conexion == null) { 
+                conexion = new SqlConnection(cadena);
+            }
+
+            if (conexion.State == System.Data.ConnectionState.Closed) 
+                conexion.Open();
+
+            return conexion;
+        }
+
+        public void CerrarConexion()
+        {
+            if (conexion != null && conexion.State == System.Data.ConnectionState.Open)
+                conexion.Close();
+        }
+    }
+}
